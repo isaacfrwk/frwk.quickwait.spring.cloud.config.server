b9e1e194bc7a7ec1ca8b5a13cf751b1e9fa802d1
Daniel Dutra <danieldhsd@gmail.com>
2022-02-16 10:15:07 -0300

    Configuring Spring Cloud Config Server

---
+++ src/test/java/com/quickwait/springconfigserver/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickwait.springconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
